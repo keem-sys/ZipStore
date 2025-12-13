efcc354eef6d7145b624c7ba5220e32938a752c2
Ebenezer <ebenkouakou7@gmail.com>
2025-12-13 17:43:52 +0200

    initial scaffold

---
+++ src/main/java/zipstore/backend/ZipstoreBackendApplication.java
@@ -0,0 +1,13 @@
+package zipstore.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZipstoreBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZipstoreBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/zipstore/backend/ZipstoreBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package zipstore.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZipstoreBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
