255cd18ecb25c9dcab4705ea9df1264e58897b60
Ebenezer <ebenkouakou7@gmail.com>
2025-12-17 15:10:32 +0200

    add OrderController

---
+++ src/main/java/zipstore/backend/controller/OrderController.java
@@ -0,0 +1,51 @@
+package zipstore.backend.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import zipstore.backend.dto.OrderRequest;
+import zipstore.backend.entity.Order;
+import zipstore.backend.entity.User;
+import zipstore.backend.repository.OrderRepository;
+import zipstore.backend.repository.UserRepository;
+import zipstore.backend.service.OrderService;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/api/orders")
+@RequiredArgsConstructor
+public class OrderController {
+
+    private final OrderRepository orderRepository;
+    private final OrderService orderService;
+    private final UserRepository userRepository;
+
+    @PostMapping
+    public ResponseEntity<Order> placeOrder(@RequestBody OrderRequest request,
+                                            Authentication authentication) {
+        String email = authentication.getName();
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found!"));
+
+        Order newOrder = orderService.placeOrder(user, request);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(newOrder);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Order>> getMyOrders(Authentication authentication) {
+        String email = authentication.getName();
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found!"));
+
+        return ResponseEntity.ok(orderService.getUsersOrders(user));
+    }
+}
