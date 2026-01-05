9d1befbf9a92b4c9d5b09ec1dd25949de94780d1
Ebenezer <ebenkouakou7@gmail.com>
2026-01-05 18:31:54 +0200

    add new url to allowed origins

--- src/main/java/zipstore/backend/config/SecurityConfig.java
+++ src/main/java/zipstore/backend/config/SecurityConfig.java
@@ -46,7 +46,7 @@ public class SecurityConfig {
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:5173"));
+        configuration.setAllowedOrigins(List.of("http://localhost:5173", "https://zipstore-shop.vercel.app/"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
 
